75030ce4f10abd2a4cc7d8fc163c0f2f8812f8e3
Pablo Alexandrino <pabloalexandrino@icloud.com>
2022-09-07 12:37:17 -0300

    Menu and Logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,11 @@ import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider defaultTheme="system">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider defaultTheme="system">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
 export default MyApp;
